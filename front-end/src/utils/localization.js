9e50c284ff56a53cacb5ad6eb48df018940bcb75
Hunter Stratton <hstratton16@gmail.com>
2021-08-24 15:23:50 -0600

    Improve localization

--- front-end/src/utils/localization.js
+++ front-end/src/utils/localization.js
@@ -1,7 +1,44 @@
-import fs from 'fs';
+class Localization {
+    constructor() {
+        this.defaultLocal = "en";
+        this.fallbackLocal = "en";
+        this.strings = {};
+        this.localizedStrings = {};
+    }
 
-export function localize(name, local) {
-    let file = fs.readFileSync("./src/resources/strings/" + name + ".json");
-    let strings = JSON.parse(file)[local];
-    return strings;
+    setDefaultLocal(local) {
+        this.defaultLocal = local;
+    }
+
+    setFallbackLocal(local) {
+        this.fallbackLocal = local;
+    }
+    
+    setStrings(strings) {
+        this.strings = strings;
+        this.localizeStrings();
+    }
+
+    localizeStrings(local=this.defaultLocal) {
+        this.localizedStrings = this.strings[this.defaultLocal]||{};
+    }
+
+    getStrings(local=this.defaultLocal) {
+        //TODO: Build strings obj using fallback local
+        this.localizeStrings(local);
+        return this.localizedStrings;
+    }
+}
+
+const _L = new Localization();
+
+export default _L;
+
+export let L = _L.localizedStrings;
+
+export const Localize = function(strings, local="en") {
+    let LC = new Localization();
+    LC.setStrings(strings);
+    LC.setDefaultLocal(local);
+    return LC.localizedStrings;
 }
