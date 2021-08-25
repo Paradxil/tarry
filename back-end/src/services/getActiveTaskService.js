5f7f6da7f26c59ba1546dacd81518be9c769cd92
Hunter Stratton <hstratton16@gmail.com>
2021-08-25 11:18:39 -0600

    Include project data with active task response.

--- back-end/src/services/getActiveTaskService.js
+++ back-end/src/services/getActiveTaskService.js
@@ -1,20 +1,30 @@
 const ActiveTaskDAO = require('../data/activeTaskDAO');
 const TaskDAO = require('../data/taskDAO');
+const ProjectDAO = require('../data/projectDAO');
 
 class GetActiveTaskService {
     async getTask(userid) {
         let dao = new ActiveTaskDAO();
         let taskDAO = new TaskDAO();
+        let projectDAO = new ProjectDAO();
 
         let activeTask = await dao.get(userid);
 
         if(activeTask !== null && activeTask.taskid !== null) {
             let task = await taskDAO.getTask(activeTask.taskid);
 
-            if(task!==null) {
+            if(task !== null) {
                 activeTask = activeTask.toJSON();
                 activeTask.name = task.name;
                 activeTask.status = task.status;
+            }
+
+            if(task.project !== null) {
+                let project = await projectDAO.get(task.project);
+
+                if(project !== null) {
+                    activeTask.project = project;
+                }
             }
         }
 
