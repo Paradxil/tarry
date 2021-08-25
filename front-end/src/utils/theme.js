022af703dc2274eeb36c888d0cbbb5db90fd9b64
Hunter Stratton <hstratton16@gmail.com>
2021-08-25 13:44:09 -0600

    Add default colors

---
+++ front-end/src/utils/theme.js
@@ -0,0 +1,14 @@
+export const colors = [
+    '#ff8787',
+    '#f783ac',
+    '#da77f2',
+    '#9775fa',
+    '#748ffc',
+    '#4dabf7',
+    '#3bc9db',
+    '#38d9a9',
+    '#69db7c',
+    '#a9e34b',
+    '#ffd43b',
+    '#ffa94d'
+]
