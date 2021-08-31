1884c483cb151cc15c4202df6b04010b65d91440
Hunter Stratton <hstratton16@gmail.com>
2021-08-30 20:42:36 -0600

    Update to use new DAO template class.

--- back-end/src/data/invoiceDAO.js
+++ back-end/src/data/invoiceDAO.js
@@ -1,30 +1,4 @@
 const Invoice = require('../model/invoice');
+const DAO = require('./DAO');
 
-class InvoiceDAO {
-    async get(id) {
-        return await Invoice.findOne({ _id: id });
-    }
-
-    async getAll(userid) {
-        return await Invoice.find({ userid: userid });
-    }
-
-    async add(data) {
-        let invoice = new Invoice(data);
-        await invoice.save();
-        return invoice;
-    }
-
-    async update(id, data) {
-        let invoice = await this.get(id);
-        for(let key of Object.keys(data)) {
-            if(key in invoice) {
-                invoice[key] = data[key];
-            }
-        }
-        await invoice.save();
-        return invoice;
-    }
-}
-
-module.exports = InvoiceDAO;
+module.exports = DAO.create(Invoice);
