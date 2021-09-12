ad1e9ac496b95fbf6cb806b97938edad316b549f
Hunter Stratton <hstratton16@gmail.com>
2021-09-12 11:16:01 -0600

    Add parenthesis.

--- front-end/src/utils/dateutils.js
+++ front-end/src/utils/dateutils.js
@@ -1,5 +1,5 @@
 export function dateInPast(daysAgo) {
-    return Date.now() - (1000 * 60 * 60 * 24 * daysAgo);
+    return (Date.now() - (1000 * 60 * 60 * 24 * daysAgo));
 }
 
 export function now() {
