b7fe7703a4b8b0cb67c964a300bb691f7955781e
Hunter Stratton <hstratton16@gmail.com>
2021-12-21 09:24:16 -0800

    Remove old migrations.

--- back-end/src/model/migrations.js
+++ back-end/src/model/migrations.js
@@ -12,102 +12,102 @@ const Task = require("./task");
 
 class Migrations {
     static MIGRATIONS = {
-        1: async function (userid) {
-            console.log("MIGRATE ADDRESS SCHEMA - ENCRYPT EXISTING DOCUMENTS");
-            const tmpAddressSchema = new mongoose.Schema({
-                userid: {type: String, required: true},
-                name: String,
-                organization: String,
-                street: String,
-                city: String,
-                state: String,
-                zip: String,
-                phone: String,
-                email: String
-            });
+        // 1: async function (userid) {
+        //     console.log("MIGRATE ADDRESS SCHEMA - ENCRYPT EXISTING DOCUMENTS");
+        //     const tmpAddressSchema = new mongoose.Schema({
+        //         userid: {type: String, required: true},
+        //         name: String,
+        //         organization: String,
+        //         street: String,
+        //         city: String,
+        //         state: String,
+        //         zip: String,
+        //         phone: String,
+        //         email: String
+        //     });
 
-            tmpAddressSchema.plugin(encrypt.migrations, { secret: config.SECRET, excludeFromEncryption: ['userid'], additionalAuthenticatedFields: ['userid'] });
-            const TmpAddress = Address.compile(Address.modelName, tmpAddressSchema, Address.collection.name, Address.db, mongoose);
-            TmpAddress.migrateToA(function(err){
-                if (err){ throw err; }
-                console.log('Migration successful');
-            });
+        //     tmpAddressSchema.plugin(encrypt.migrations, { secret: config.SECRET, excludeFromEncryption: ['userid'], additionalAuthenticatedFields: ['userid'] });
+        //     const TmpAddress = Address.compile(Address.modelName, tmpAddressSchema, Address.collection.name, Address.db, mongoose);
+        //     TmpAddress.migrateToA(function(err){
+        //         if (err){ throw err; }
+        //         console.log('Migration successful');
+        //     });
 
-            let userDAO = new UserDAO();
-            let user = await userDAO.getUserById(userid);
-            user.schemaVersion = 2;
-            await user.save();
-        },
-        2: async function (userid) {
-            console.log("MIGRATE PROJECT SCHEMA - ENCRYPT EXISTING DOCUMENTS");
-            const tmpProjectSchema = new mongoose.Schema({
-                name: {type: String, required: true},
-                userid: {type: String, required: true},
-                color: {type: String, required: true},
-                wage: {type: Number, required: true}
-            });
+        //     let userDAO = new UserDAO();
+        //     let user = await userDAO.getUserById(userid);
+        //     user.schemaVersion = 2;
+        //     await user.save();
+        // },
+        // 2: async function (userid) {
+        //     console.log("MIGRATE PROJECT SCHEMA - ENCRYPT EXISTING DOCUMENTS");
+        //     const tmpProjectSchema = new mongoose.Schema({
+        //         name: {type: String, required: true},
+        //         userid: {type: String, required: true},
+        //         color: {type: String, required: true},
+        //         wage: {type: Number, required: true}
+        //     });
 
-            tmpProjectSchema.plugin(encrypt.migrations, { secret: config.SECRET, excludeFromEncryption: ['userid'], additionalAuthenticatedFields: ['userid'] });
-            const TmpProject = Project.compile(Project.modelName, tmpProjectSchema, Project.collection.name, Project.db, mongoose);
-            TmpProject.migrateToA(function(err){
-                if (err){ throw err; }
-                console.log('Migration successful');
-            });
+        //     tmpProjectSchema.plugin(encrypt.migrations, { secret: config.SECRET, excludeFromEncryption: ['userid'], additionalAuthenticatedFields: ['userid'] });
+        //     const TmpProject = Project.compile(Project.modelName, tmpProjectSchema, Project.collection.name, Project.db, mongoose);
+        //     TmpProject.migrateToA(function(err){
+        //         if (err){ throw err; }
+        //         console.log('Migration successful');
+        //     });
 
-            let userDAO = new UserDAO();
-            let user = await userDAO.getUserById(userid);
-            user.schemaVersion = 3;
-            await user.save();
-        },
-        4: async function (userid) {
-            console.log("MIGRATE TASK SCHEMA - ENCRYPT EXISTING DOCUMENTS");
-            const tmpTaskSchema = new mongoose.Schema({
-                name: {type: String, required: true},
-                userid: {type: String, required: true},
-                status: {type: String, required: true, enum: ["untracked", "todo", "completed"], default: "untracked"},
-                project: {        
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'Projects',
-                    autopopulate: true
-                }
-            });
+        //     let userDAO = new UserDAO();
+        //     let user = await userDAO.getUserById(userid);
+        //     user.schemaVersion = 3;
+        //     await user.save();
+        // },
+        // 4: async function (userid) {
+        //     console.log("MIGRATE TASK SCHEMA - ENCRYPT EXISTING DOCUMENTS");
+        //     const tmpTaskSchema = new mongoose.Schema({
+        //         name: {type: String, required: true},
+        //         userid: {type: String, required: true},
+        //         status: {type: String, required: true, enum: ["untracked", "todo", "completed"], default: "untracked"},
+        //         project: {        
+        //             type: mongoose.Schema.Types.ObjectId,
+        //             ref: 'Projects',
+        //             autopopulate: true
+        //         }
+        //     });
 
-            tmpTaskSchema.plugin(encrypt.migrations, {secret: process.env.SECRET, encryptedFields: ['name']});
-            const TmpTask = Task.compile(Task.modelName, tmpTaskSchema, Task.collection.name, Task.db, mongoose);
-            TmpTask.migrateToA(function(err){
-                if (err){ throw err; }
-                console.log('Migration successful');
-            });
+        //     tmpTaskSchema.plugin(encrypt.migrations, {secret: process.env.SECRET, encryptedFields: ['name']});
+        //     const TmpTask = Task.compile(Task.modelName, tmpTaskSchema, Task.collection.name, Task.db, mongoose);
+        //     TmpTask.migrateToA(function(err){
+        //         if (err){ throw err; }
+        //         console.log('Migration successful');
+        //     });
 
-            // let taskDAO = new TaskDAO();
-            // let tasks = await taskDAO.getAllTasks(userid);
+        //     // let taskDAO = new TaskDAO();
+        //     // let tasks = await taskDAO.getAllTasks(userid);
 
-            // let taskNum = 0;
+        //     // let taskNum = 0;
 
-            // for(let task of tasks) {
-            //     try {
-            //         task.decrypt(async (err) => {
-            //             if(err) {
-            //                 console.log(err);
-            //                 return;
-            //             }
+        //     // for(let task of tasks) {
+        //     //     try {
+        //     //         task.decrypt(async (err) => {
+        //     //             if(err) {
+        //     //                 console.log(err);
+        //     //                 return;
+        //     //             }
 
-            //             task.name = "TASK " + taskNum;
-            //             taskNum += 1;
-            //             await task.save();
+        //     //             task.name = "TASK " + taskNum;
+        //     //             taskNum += 1;
+        //     //             await task.save();
 
-            //         });
-            //     }
-            //     catch(err) {
-            //         console.log(err);
-            //     }
-            // }
+        //     //         });
+        //     //     }
+        //     //     catch(err) {
+        //     //         console.log(err);
+        //     //     }
+        //     // }
 
-            let userDAO = new UserDAO();
-            let user = await userDAO.getUserById(userid);
-            user.schemaVersion = 5;
-            await user.save();
-        }
+        //     let userDAO = new UserDAO();
+        //     let user = await userDAO.getUserById(userid);
+        //     user.schemaVersion = 5;
+        //     await user.save();
+        // }
     }
 
     static ShouldMigrate(version) {
